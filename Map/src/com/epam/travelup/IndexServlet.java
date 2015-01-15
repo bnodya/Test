0d852a934681f8a003a937398ce3a6820c053e07
bnodya <b.shamanovskyi@gmail.com>
2015-01-15 18:29:39 +0200

    comments rermoved

--- Map/src/com/epam/travelup/IndexServlet.java
+++ Map/src/com/epam/travelup/IndexServlet.java
@@ -19,18 +19,11 @@ import javax.servlet.http.HttpServletResponse;
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public IndexServlet() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		ResultSet rs = null;
